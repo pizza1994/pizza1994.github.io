ba9fa2844c0cb3d487f23fd1c6d65df1fa236ab1
pizza1994 <lucapizza@gmail.com>
2021-11-03 12:02:42 +0100

    uploading files

---
+++ gatsby-config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  pathPrefix: "/pizza1994.github.io",
+  siteMetadata: {
+    description: "Personal page of Luca Pitzalis",
+    locale: "en",
+    title: "Luca Pitzalis",
+    showThemeLogo: true,
+    formspreeEndpoint: "https://formspree.io/f/moqyowly",
+  },
+  plugins: [
+    {
+      resolve: "@wkocjan/gatsby-theme-intro",
+      options: {
+        theme: "gh-inspired",
+      },
+    },
+  ],
+}
